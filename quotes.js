f2fc4aac3d0298538aacc8af6e9dc7c06044edbe
Asia Watts <wattsasiak@gmail.com>
2021-02-04 19:51:43 -0500

    final change

--- quotes.js
+++ quotes.js
@@ -7,7 +7,7 @@ const quotes = [
     "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty. – Winston Churchill",
     "Don’t Let Yesterday Take Up Too Much Of Today. – Will Rogers",
     "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character. – Unknown",
-    "Its Not Whether You Get Knocked Down, It’s Whether You Get Up.  By Vince Lombardi",
+    "Its Not Whether You Get Knocked Down, It’s Whether You Get Up.  - Vince Lombardi",
     "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You. – Steve Jobs",
     "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do. – Rob Siltane",
 
