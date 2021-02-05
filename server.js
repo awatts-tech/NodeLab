6791f80b3ace8b4c40f0c4027016ab79c0f40d45
Asia Watts <wattsasiak@gmail.com>
2021-02-04 19:33:10 -0500

    Pushing final changes

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 "use strict";
 
-const {prototype} = require("events");
+
+const quotes = require("./quotes.js")
+
 const http = require('http');
 const port = 3000;
 
 
-
-const quotes = [
-    "Be yourself; everyone else is already taken. -Oscar Wilde",
-    "You only live once, but if you do it right, once is enough. - Mae West",
-    "Be the change that you wish to see in the world. - Mahatma Gandhi"
-];
-
-
-const index = Math.floor(Math.random() *quotes.length);
-const randomQuote = quotes[index];
-
 http.createServer((req, res) => {
-   
+    const index = Math.floor(Math.random() *quotes.length);
+    const randomQuote = quotes[index];  
+    console.log(randomQuote);
  
 res.writeHead(200, {"Content-type": "text/plain"});
 res.write(randomQuote);
 
 res.end();
 }).listen(port);
-// console.log(randomQuote);
 
